9f1a8b546e89682496933e114e61cac5fd10dc11
Victor Hugo <Vhugo7523@gmail.com>
2025-05-24 15:41:57 -0300

    update 24/05/25

--- exercises/ex057.py
+++ exercises/ex057.py
@@ -1,5 +1,5 @@
-sexo = input('Digite o sexo (M/F): ').strip() .upper()
+sexo = input('Digite o sexo (M/F): ').strip() .upper()[0]
 while sexo not in ['M', 'F']:
     print('Valor inválido, por favor, digite M ou F!')
-    sexo = input('Digite o sexo (M/F): ').strip() .upper()
+    sexo = input('Digite o sexo (M/F): ').strip() .upper()[0]
 print('Sexo {} registrado com sucesso.'.format(sexo))
