26151ab502b30fa337ac450bee15811d98ea9f65
Victor Hugo <Vhugo7523@gmail.com>
2025-05-26 19:09:23 -0300

    update 26/05/25

--- exercises/ex065.py
+++ exercises/ex065.py
@@ -1,25 +1,17 @@
-soma = 0
-contador = 0
-maior = None
-menor = None
-
-while True:
-    numero = int(input('Digite um número inteiro: '))
-    
-    soma += numero
-    contador += 1
-    
-    if maior is None or numero > maior:
-        maior = numero
-    if menor is None or numero < menor:
-        menor = numero
-    
-    resposta = input('Quer continuar? [S/N] ').strip().upper()
-    if resposta != 'S':
-        break
-
-media = soma / contador if contador > 0 else 0
-
-print(f'Média dos números digitados: {media}')
-print(f'Maior número digitado: {maior}')
-print(f'Menor número digitado: {menor}')
+resp = 'S'
+soma = quant = media = maior = menor = 0
+while resp in 'Ss':
+    num = int(input('Digite um número: '))
+    soma += num
+    quant += 1
+    if quant == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+    resp = str(input('Quer continuar? [S/N]')).upper() .strip() [0]
+media = soma / quant
+print('Você digitou {} números e a média foi {}'.format(quant, media))
+print('O maior valor foi {} e o menor foi {}'.format(maior, menor))
