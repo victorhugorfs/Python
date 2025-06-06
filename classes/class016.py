9d61f204bdad91ee3047e826eb184cbf8604aeb4
Victor Hugo <Vhugo7523@gmail.com>
2025-06-05 22:56:38 -0300

    update 05/06/25

---
+++ classes/class016.py
@@ -0,0 +1,83 @@
+#Tuplas são imutáveis!
+
+#lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+#print (lanche)
+
+#lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+#print (lanche[1])
+
+#lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+#print (lanche[-2])
+
+#lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+#print (lanche[1:3])
+
+#lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+#print (lanche[2:])
+
+#lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+#print (lanche[:2])
+
+#lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+#print (lanche[-3:])
+
+#lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+#lanche[1] = 'Refrigerante'     ERRO! "Tuplas são imutáveis!"
+#print (lanche
+
+#lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+#for comida in lanche:
+#    print(f'Eu vou comer {comida}')
+#print('Comi pra caramba!')
+
+#lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+#print(len(lanche))
+#print('Comi pra caramba!')
+
+#lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+#for cont in range(0, len(lanche)):
+#    print(lanche[cont])
+#print('Comi pra caramba!')
+
+#lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+#for cont in range(0, len(lanche)):
+#    print(f'Eu vou comer {lanche[cont]} na posição {cont}')
+#print('Comi pra caramba!')
+
+#for pos, comida in enumerate(lanche):
+#    print(f'Eu vou comer {comida} na posição {pos}')
+#print('Comi pra caramba!')
+
+#lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+#print(sorted(lanche))
+
+#a = (2, 5, 4)
+#b = (5, 8, 1, 2)
+#c = a + b
+#print(c)
+
+#a = (2, 5, 4)
+#b = (5, 8, 1, 2)
+#c = a + b
+#print(len(c))
+
+#a = (2, 5, 4)
+#b = (5, 8, 1, 2)
+#c = a + b
+#print(c.count(5))
+
+#a = (2, 5, 4)
+#b = (5, 8, 1, 2)
+#c = a + b
+#print(c)
+#print(c.index(8))
+
+#a = (2, 5, 4)
+#b = (5, 8, 1, 2)
+#c = a + b
+#print(c)
+#print(c.index(5, 1))
+
+#pessoa = ('Victor', 18, 'Masculino', 84.00)
+#del(pessoa)
+#print(pessoa)
