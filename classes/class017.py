cf7483f546d136742efdc93bd28f2a1fcf06949c
Victor Hugo <Vhugo7523@gmail.com>
2025-06-16 18:17:47 -0300

    update 06/16/25

---
+++ classes/class017.py
@@ -0,0 +1,36 @@
+#num = [4, 2, 8, 6, 10]
+#num [2] = 5
+#num.append(12)
+#num.sort()
+#num.sort(reverse=True)
+#num.insert(2, 0)
+#num.pop(2)
+#if 13 in num:
+#    num.remove(13)
+#else:
+#    print('I didnt found the number 13')
+#print(num)
+#print(f'This list have {len(num)} elements.')
+
+#value = []
+#value.append(5)
+#value.append(2)
+#value.append(8)
+#for v in value:
+#    print(f'{v}...', end=' ')
+#for c, v in enumerate(value):
+#    print(f'In position {c} i found the value {v}!')
+#print('I reached the end of the list.')
+
+#value = list()
+#for cont in range(0, 5):
+#    value.append(int(input('Type 5 different numbers: ')))
+#for c, v in enumerate(value):
+#    print(f'In position {c} i found the value {v}!')
+#print('I reached the end of the list.')
+
+#a = [2, 3, 4, 7]
+#b = a[:]                          # b = a "This is a call" b = a[:] "This is a copy"
+#b[2] = 8    
+#print(f'List A: {a}')
+#print(f'List B: {b}')
