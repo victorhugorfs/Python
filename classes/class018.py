ec0e78c20bb1308d029f84fbb0635f5f0a3c2132
Victor Hugo <Vhugo7523@gmail.com>
2025-06-24 11:56:22 -0300

    update 06/24/25

---
+++ classes/class018.py
@@ -0,0 +1,36 @@
+#test = list()
+#test.append('Victor')
+#test.append(18)
+#guys = list()
+#guys.append(test[:])
+#test[0] = 'Giovana'
+#test[1] = 18
+#guys.append(test[:])
+#print(guys)
+
+
+#guys = [['Victor', 18], ['Arthur', 19], ['Gilson', 19], ['Renan', 19]]
+#for people in guys:
+    #print(people)
+    #print(people[0])
+    #print(people[1])
+    #print(f'{people[0]} is {people[1]} years old')
+
+
+#guys = list()
+#date = list()
+#max = min = 0
+#for c in range(5):
+    #date.append(str(input('Name: ')))
+    #date.append(int(input('Age: ')))
+    #guys.append(date[:])
+    #date.clear()
+#for people in guys:
+    #if people[1] >= 21:
+        #print(f'{people[0]} and Older')
+        #max += 1
+    #else:
+        #print(f'{people[0]} is under age!')
+        #min += 1
+
+#print(f'We have {max} older and {min} under age!')
