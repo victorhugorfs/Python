9655312c955066e44dd62d74d5aa5d191dfe3294
Victor Hugo <Vhugo7523@gmail.com>
2025-07-01 10:43:44 -0300

    update 07/01/25

---
+++ classes/class019.py
@@ -0,0 +1,39 @@
+#people = {'name': 'Victor', 'sex': 'M', 'age': 18}
+#print(people['name'])
+#print(f'{people["name"]} is {people["age"]} years old!')
+#print(people.keys())
+#print(people.values())
+#print(people.items())
+#for k in people.keys():
+    #print(k)
+#for k in people.values():
+    #print(k)
+#for k, v in people.keys():
+    #print(f'{k} = {v}')
+
+#people = {'name': 'Victor', 'sex': 'M', 'age': 18}
+#del people['sex']
+#for k, v in people.keys():
+    #print(f'{k} = {v}')
+
+#people = {'name': 'Victor', 'sex': 'M', 'age': 18}
+#people['name'] = 'Arthur'
+#for k, v in people.keys():
+    #print(f'{k} = {v}')
+
+#usa = []
+#city1 = {'fu': 'Orlando', 'acronym': 'MCO'}
+#city2 = {'fu': 'Los Angeles', 'aconym': 'LA'}
+#usa.append(city1)
+#usa.append(city2)
+#print(usa) or print(usa[1]) or print(usa[0]['fu'])
+
+#state = dict()
+#usa = list()
+#for c in range(3):
+    #state['fu'] = str(input('Federative Unit: '))
+    #state['acronym'] = str(input('State acronym: '))
+    #usa.append(state.copy())
+#for s in usa:
+    #for k, v in s.items():
+        #print(f'Field {k} has value {v}')
