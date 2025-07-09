c4f32ee47e6c5f002932ee8a93b036006540d46e
Victor Hugo <Vhugo7523@gmail.com>
2025-07-09 11:06:49 -0300

    update 07/09/25

---
+++ classes/class020.py
@@ -0,0 +1,59 @@
+#def sum(a, b):
+    #s = a + b
+    #print(s)
+
+#sum(4, 5)
+#sum(8, 9)
+#sum(2, 1)
+
+
+#def sum(a, b):
+    #print(f'A = {a} and B = {b}')
+    #s = a + b
+    #print(f'The sum A + B = {s}')
+
+#sum(a=4, b=5)
+#sum(a=5, b=4)
+#sum(7, 2)
+
+
+#def cont(* num):
+    #for value in num:
+        #print(value, end='')
+
+
+#cont(2, 1, 7)
+#cont(5, 3, 2, 6)
+#cont(1, 5, 7, 2, 4)
+
+
+#def cont(* num):
+    #size = len(num)
+    #print(f'I received the values ​​{num} and there are a total of {size} numbers')
+
+#cont(5, 2, 6)
+#cont(2, 0)
+#cont(6, 9, 0, 2)
+
+
+#def double(list):
+    #pos = 0
+    #while pos < len(list):
+        #list[pos] *= 2
+        #pos += 1
+
+
+#values = [2, 4, 6, 2, 1, 6,]
+#double(values)
+#print(values) 
+
+
+#def sum(* values):
+    #s = 0
+    #for num in values:
+        #s += num
+    #print(f'Sum all values {values} we have {s}')
+
+
+#sum(5, 2)
+#sum(3, 5, 7)
