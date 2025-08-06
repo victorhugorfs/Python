f70e3e9d82592c16acd476a4d81a329bec12a56a
Victor Hugo <Vhugo7523@gmail.com>
2025-08-06 14:14:38 -0300

    update 08/06/25 class 021

---
+++ classes/class021.py
@@ -0,0 +1,33 @@
+#def factorial (num=1):
+    #f = 1
+    #for c in range(num, 0, -1):
+        #f *= c
+    #return f
+
+#n = int(input('Type one number: '))
+#print(f'The factorial of {n} it is equal at {factorial(n)}')
+
+
+#def factorial (num=1):
+    #f = 1
+    #for c in range(num, 0, -1):
+        #f *= c
+    #return f
+
+#f1 = factorial(5)
+#f2 = factorial(4)
+#f3 = factorial()
+#print(f'The results it is {f1}, {f2} and {f3}')
+
+
+#def even (n=0):
+    #if n % 2 == 0:
+        #return True
+    #else:
+        #return False
+    
+#num = int(input('Type one number: '))
+#if even(num):
+    #print('It is even!')
+#else:
+    #print('It is not even.')
